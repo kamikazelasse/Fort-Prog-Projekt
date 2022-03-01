6f0e70743026b76464350d73f8af37a33d481757
kamikazelasse <lasse.wehr@gmail.com>
2022-03-01 12:24:36 +0100

    v2 task4

--- Task4.hs
+++ Task4.hs
@@ -1,35 +1,47 @@
 module Task4 where
 
-import Type ( Term(Comb), VarName ) 
+import Type ( Term(Var ,Comb), VarName (VarName) ) 
 import Task3 ( Vars(allVars), contains )
 import Data.List (delete)
 
-data Subst = Subst [VarName] Term 
+data Subst = Subst [VarName] [Term] 
+ deriving Show
 
 domain :: Subst -> [VarName]
-domain (Subst vars term) =  without vars (allVars term)
+domain (Subst [] []) = []
+domain (Subst (v:vs) (t:ts)) = if( [v] == allVars t ) 
+                                  then domain (Subst vs ts) 
+                                  else (v:domain (Subst vs ts))
 
 empty :: Subst
-empty = Subst [] (Comb "" []) -- ?
+empty = Subst [] []
 
 single :: VarName -> Term -> Subst
-single name term = Subst [name] term
+single name term = Subst [name] [term]
 
 apply :: Subst -> Term -> Term
-apply (Subst [] _ ) term2 = term2
-apply (Subst list term1) term2 = if(allContains (allVars term2) list) 
-                                    then term1 
-                                    else term2
+apply (Subst [] [] ) term2 = term2
+apply (Subst (n:ns) (t:ts)) (Var x) = if(n == x) then t else apply (Subst ns ts) (Var x)
+apply subst (Comb s terms) = Comb s (map (\x -> apply subst x) terms)
+
 
 compose :: Subst -> Subst -> Subst
-compose sub1 sub2 = sub1                                    
+compose  subst1 (Subst list2 terms2 )= addit (Subst list2 (map (\t -> apply subst1 t) terms2 )) subst1 
+ where 
+        addit :: Subst -> Subst -> Subst
+        addit (Subst [] []) subst2 = subst2
+        addit (Subst vars1 terms1) (Subst vars2 terms2) = Subst (vars1 ++ vars2) (terms1 ++ terms2)
+        
+                                    
 
+--           this without this   
 without ::  [VarName] -> [VarName] -> [VarName] 
+without list [] = list
 without list (r:rs) = if (contains r list) 
                             then without (delete r list) rs 
                             else without list rs
 
 --  true if    this \/ is in this \/
 allContains :: [VarName] -> [VarName] -> Bool
-allCantains []  _ = True 
+allContains []  _ = True
 allContains (x:xs) list = if( contains x list ) then allContains xs list else False 
