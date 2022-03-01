64571722cc1aee4489ae1b7c14614bc92c6d5889
kamikazelasse <lasse.wehr@gmail.com>
2022-03-01 19:42:46 +0100

    bonusaufgabe 1

--- Task2.hs
+++ Task2.hs
@@ -9,7 +9,20 @@ class Pretty a where
 instance Pretty Term where
     pretty (Var (VarName s)) = s 
     pretty (Comb s []) =  s
-    pretty (Comb s (t:ts)) = s ++ "(" ++ pretty t ++ foldr (\a p -> ", " ++ pretty a ++ p ) "" ts  ++")"
+    pretty (Comb s (t:ts)) = if isPrologList (Comb s (t:ts))
+                            then "[" ++ makeList (t:ts) "" ++ "]"
+                            else s ++ "(" ++ pretty t ++ foldr (\a p -> ", " ++ pretty a ++ p ) "" ts  ++")"
+     where 
+         isPrologList (Comb "." list) = length list == 2
+         isPrologList (Comb _ list) = False
+
+         makeList [(Var s), term2] done = pretty (Var s) ++ "|" ++ pretty term2 ++ done
+         makeList [ term , (Comb "[]" [])] done = pretty term ++ done
+         makeList [ term , (Comb "." list) ] done = 
+             if isPrologList (Comb "." list) 
+                 then pretty term ++ ", " ++ makeList list done 
+                 else pretty term ++ ", " ++ pretty (Comb "." list)    
+         makeList [term , term2] done = pretty term ++ "|" ++ pretty term2 ++ done
 
 instance Pretty Rule where
     pretty (Rule term []) = pretty term 
