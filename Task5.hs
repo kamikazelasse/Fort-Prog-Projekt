5d238b820293407d504a85232241c4d2a2543604
LeifIsStrange <leif.looks@yahoo.com>
2022-03-02 10:35:19 +0100

    henlo

--- Task5.hs
+++ Task5.hs
@@ -4,13 +4,11 @@ import Type ( Term(..) )
 
 
 ds :: Term -> Term -> Maybe (Term, Term)
-ds (Var s1) (Comb s2 terms) = if ((Var s1) == (Comb s2 terms)) 
-                                then fall3 (Var s1) (Comb s2 terms) 
-                                else Just ((Var s1), (Comb s2 terms))
-ds (Comb s1 terms) (Var s2) = if ((Comb s1 terms) == (Var s2))   
-                                then fall3 (Comb s1 terms) (Var s2) 
-                                else Just ((Comb s1 terms), (Var s2))    
-ds term1 term2  = if ( term1 == term2) then Nothing else fall3 term1 term2
+ds (Var s1) (Comb s2 terms) =  Just ((Var s1), (Comb s2 terms))
+ds (Comb s1 terms) (Var s2) = Just ((Comb s1 terms), (Var s2))  
+ds term1 term2  = if ( term1 == term2) 
+                    then Nothing
+                    else fall3 term1 term2
  where       
      fall3 :: Term -> Term -> Maybe (Term, Term)
      fall3 (Comb s1 (t1:t1s)) (Comb s2 (t2:t2s)) 
@@ -18,4 +16,6 @@ ds term1 term2  = if ( term1 == term2) then Nothing else fall3 term1 term2
                                                                 then  Just (t1, t2) 
                                                                 else fall3 (Comb s1 t1s) (Comb s2 t2s)
       | otherwise = Just ((Comb s1 (t1:t1s)), (Comb s2 (t2:t2s)))
+     fall3 t1 t2 = Just(t1, t2)
 
+unify :: Term -> Term -> Maybe Subst
