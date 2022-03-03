45476c58654d361a530616b49dcec9fea8c0a424
kamikazelasse <lasse.wehr@gmail.com>
2022-03-04 00:43:51 +0100

    nacht weil ging nicht aus dem kopf

--- Task7.hs
+++ Task7.hs
@@ -1,28 +1,33 @@
 module Task7 where
 import Task41 ( apply, empty, Subst (Subst) )
-import Type ( Goal(..), Prog(..), Rule(..), Term, VarName )
+import Type ( Goal(..), Prog(..), Rule(..), Term (Var, Comb), VarName(VarName) )
 import Task5 ( unify, isNothing )
 import Task3 ( Vars(allVars) )
+import Task6 ( rename )
+import Task2 ( Pretty(..) )
 
 data SLDTree = SLDTree Goal [(Subst,SLDTree)] 
+ deriving Show
 
 sldLeaf :: Goal -> SLDTree
 sldLeaf goal = SLDTree goal []
 
 sld :: Prog -> Goal -> SLDTree
-sld prog goal = sldOhne prog goal []
- where 
-  sldOhne :: Prog -> Goal -> [VarName] -> SLDTree
-  sldOhne (Prog []) goal _ = sldLeaf goal
-  sldOhne (Prog rules) (Goal tems) ohne = SLDTree goal  (unifyRules rules tems (ohne ++ (allVars (Prog rules)) ++ (allVars (Goal tems))))
+sld prog goal = sldOhne prog goal ( allVars goal)
+ 
+sldOhne :: Prog -> Goal -> [VarName] -> SLDTree
+sldOhne (Prog []) goal _ = sldLeaf goal
+sldOhne (Prog rules) (Goal tems) ohne = SLDTree (Goal tems) (unifyRules (map (\r -> rename ohne r)  rules) tems  0)
 
 
 
-unifyRules ::  [Rule] -> [Term] -> [VarName] -> [(Subst, SLDTree)]
-unifyRules  _ [] _ = []
-unifyRules  rules (t:ts) list = if canApplyRules rules t
-    then  error (" nicht lösbar ? ")
-    else  [(getSubst rules t , sld (Prog rules)  (Goal ((applyRule rules t) ++ (map (\x -> apply (getSubst rules t) x) (ts)))))] ++ unifyRules rules (t:ts) list
+unifyRules ::  [Rule] -> [Term] -> Int -> [(Subst, SLDTree)]
+unifyRules  _ [] _  = []
+unifyRules  rules (t:ts) n = if n >= length rules
+    then [] 
+    else if canApplyRule (rules !! n) t
+            then  [(getSubst rules t , sld (Prog rules)  (Goal ((applyRule (rules !! n) t) ++ ts)))] ++ unifyRules (map (\r -> rename []r) rules)  (t:ts) (n+1)
+            else  unifyRules rules (t:ts)  (n+1)
 
 
 getSubst :: [Rule] -> Term -> Subst 
@@ -35,14 +40,35 @@ getSubst  ((Rule term res): rules) t = if isNothing (unify term t)
         getJust (Just subst) = subst
         getJust Nothing = empty 
 
-applyRule :: [Rule] -> Term -> [Term] 
-applyRule [] t = [t]
-applyRule ((Rule term res): rules) t = if isNothing (unify term t) 
-    then applyRule rules t
+applyRule :: Rule -> Term -> [Term] 
+applyRule (Rule term res) t = if isNothing (unify term t) 
+    then error (" error ")
     else res 
 
-canApplyRules :: [Rule] -> Term -> Bool 
-canApplyRules [] _ = False
-canApplyRules ((Rule term res): rules) t = if isNothing (unify term t) 
-    then canApplyRules rules t
-    else True
+canApplyRule :: Rule -> Term -> Bool 
+canApplyRule (Rule term res) t = not (isNothing (unify term t))
+
+instance Pretty SLDTree where
+    pretty tree = make tree 0
+     where
+         make :: SLDTree ->Int -> String
+         make (SLDTree goal []) n = repeat "|   " n ++ pretty goal ++ "\n"
+         make (SLDTree goal (branch)) n = repeat "|   " n ++ pretty goal ++ "\n" ++ makeBranch branch n
+
+         makeBranch :: [(Subst , SLDTree)] -> Int -> String
+         makeBranch [] n = ""
+         makeBranch ((subst,tree):other ) n = repeat "|   " n ++ "+-- " ++ pretty subst ++ "\n" ++ make tree (n+1) ++ makeBranch other n
+
+         repeat :: String -> Int -> String 
+         repeat _ 0 = ""
+         repeat s n = s++ repeat s (n-1)   
+
+
+p :: Prog
+p = Prog [Rule (Comb "append" [Var (VarName "[]"), Var (VarName "Ys"), Var (VarName "Ys")]) [], Rule (Comb "append" [Comb "." [Var (VarName "X"), Var (VarName "Xs")], Var (VarName "Ys"), Comb "." [Var (VarName "X"), Var (VarName "Zs")]]) [Comb "append" [Var (VarName "Xs"), Var (VarName "Ys"), Var (VarName "Zs")]]]
+
+g :: Goal
+g = Goal [Comb "append" [Var (VarName "X"), Var (VarName "Y"), Comb "." [Comb "1" [], Comb "[]" []]]]
+
+g2 :: Goal
+g2 = Goal [Comb "append" [Var (VarName "X"), Var (VarName "Y"), Var (VarName "Z")]]
